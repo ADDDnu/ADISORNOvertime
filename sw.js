a03269fac9910a3da40049cd9af034c191857561
ADDDnu <92780801+ADDDnu@users.noreply.github.com>
2025-09-07 18:03:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const CACHE='ot-manual-v7'; self.addEventListener('install',()=>self.skipWaiting());
+const CACHE = 'ot-manual-v10';
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.webmanifest']))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
