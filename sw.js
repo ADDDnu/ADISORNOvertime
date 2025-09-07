5a79d2c69505f4800a0e0686b0e22b958a5ea468
ADDDnu <92780801+ADDDnu@users.noreply.github.com>
2025-09-07 18:36:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,9 @@
-const CACHE = 'ot-app-v11';
-const ASSETS = [
-  './',
-  './login.html',
-  './index.html',
-  './manifest.webmanifest',
-  './js/login.js',
-  './js/dashboard.js',
-  './assets/app_icon.png',
-  './assets/user_photo.jpeg'
+const CACHE='ot-app-v12';
+const ASSETS=[
+  './','./login.html','./index.html','./manifest.webmanifest',
+  './js/login.js','./js/app.js',
+  './assets/app_icon.png','./assets/user_photo.jpeg'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
-});
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))); });
