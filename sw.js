8883b69066c21f7bf92c110d0d560ed1ed401c7c
ADDDnu <92780801+ADDDnu@users.noreply.github.com>
2025-10-13 17:50:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-const CACHE = 'ot-app-v17';
-const ASSETS = [
+const CACHE='ot-app-v17-9';
+const ASSETS=[
   './',
   './index.html',
   './login.html',
-  './js/app.js',
-  './js/login.js',
+  './js/app.js?v=17.9',
+  './js/login.js?v=17.9',
   './manifest.webmanifest',
   './assets/app_icon.png'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
-});
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
